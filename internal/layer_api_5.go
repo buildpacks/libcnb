982e2d490973a8451801c02a99ca63a9d12af32d
Daniel Mikusa <dmikusa@vmware.com>
2021-04-19 09:27:36 -0400

    Added comment to explain coding decision of inlining bools
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>

--- internal/layer_api_5.go
+++ internal/layer_api_5.go
@@ -2,6 +2,8 @@ package internal
 
 // LayerAPI5 is used for backwards compatibility with serialization/deserialization of the layer toml
 type LayerAPI5 struct {
+	// bool's are inlined to instead of using libcnb.LayerTypes to break a circular package reference, internal -> libcnb -> internal -> ...
+
 	// Build indicates that a layer should be used for builds.
 	Build bool `toml:"build"`
 
