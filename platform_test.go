782a4fffeb838e9b7082a1dc631287df383c4991
Johannes Dillmann <modulo11@users.noreply.github.com>
2023-09-21 19:10:44 +0200

    Convert nested binding credentials to JSON (#256)
    Signed-off-by: Johannes Dillmann <j.dillmann@sap.com>
    Signed-off-by: Pavel Busko <pavel.busko@sap.com>
    Co-authored-by: Pavel Busko <pavel.busko@sap.com>
    Co-authored-by: Johannes Dillmann <j.dillmann@sap.com>
    Signed-off-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- platform_test.go
+++ platform_test.go
@@ -50,12 +50,38 @@ func testPlatform(t *testing.T, context spec.G, it spec.S) {
 				bindings, err := libcnb.NewBindingsForBuild("")
 				Expect(err).NotTo(HaveOccurred())
 
-				Expect(bindings).To(HaveLen(2))
-
-				types := []string{bindings[0].Type, bindings[1].Type}
-				Expect(types).To(ContainElements("elephantsql-type", "sendgrid-type"))
-				providers := []string{bindings[0].Provider, bindings[1].Provider}
-				Expect(providers).To(ContainElements("elephantsql-provider", "sendgrid-provider"))
+				Expect(bindings).To(ConsistOf(libcnb.Bindings{
+					{
+						Name:     "elephantsql-binding-c6c60",
+						Type:     "elephantsql-type",
+						Provider: "elephantsql-provider",
+						Secret: map[string]string{
+							"bool": "true",
+							"int":  "1",
+							"uri":  "postgres://exampleuser:examplepass@postgres.example.com:5432/exampleuser",
+						},
+					},
+					{
+						Name:     "mysendgrid",
+						Type:     "sendgrid-type",
+						Provider: "sendgrid-provider",
+						Secret: map[string]string{
+							"username": "QvsXMbJ3rK",
+							"password": "HCHMOYluTv",
+							"hostname": "smtp.example.com",
+						},
+					},
+					{
+						Name:     "postgres",
+						Type:     "postgres",
+						Provider: "postgres",
+						Secret: map[string]string{
+							"urls":     "{\"example\":\"http://example.com\"}",
+							"username": "foo",
+							"password": "bar",
+						},
+					},
+				}))
 			})
 
 			it("creates empty bindings from empty VCAP_SERVICES", func() {
@@ -77,9 +103,9 @@ func testPlatform(t *testing.T, context spec.G, it spec.S) {
 				bindings, err := libcnb.NewBindingsForLaunch()
 				Expect(err).NotTo(HaveOccurred())
 
-				Expect(bindings).To(HaveLen(2))
-				types := []string{bindings[0].Type, bindings[1].Type}
-				Expect(types).To(ContainElements("elephantsql-type", "sendgrid-type"))
+				Expect(bindings).To(HaveLen(3))
+				types := []string{bindings[0].Type, bindings[1].Type, bindings[2].Type}
+				Expect(types).To(ContainElements("elephantsql-type", "sendgrid-type", "postgres"))
 			})
 
 			it("creates empty bindings from empty VCAP_SERVICES", func() {
